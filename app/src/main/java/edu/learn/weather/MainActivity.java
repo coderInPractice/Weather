ea2fa0c6fa46ba94bf0652743b563de101585abc
Manish Verma <manish.verma604@gmail.com>
2022-09-27 23:54:03 +0530

    Main screen UI created and related resources added.

--- app/src/main/java/edu/learn/weather/MainActivity.java
+++ app/src/main/java/edu/learn/weather/MainActivity.java
@@ -1,21 +1,8 @@
 package edu.learn.weather;
 
-import android.Manifest;
-import android.content.Context;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.TextView;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,150 +15,11 @@ public class MainActivity extends AppCompatActivity {
     // Distance between location updates (1000m or 1km)
     final float MIN_DISTANCE = 1000;
 
-    //final String CITY = "VARANASI";
-
-    final int REQUEST_CODE = 123;
-    // TODO: Set LOCATION_PROVIDER here:
-    String LOCATION_PROVIDER = LocationManager.GPS_PROVIDER;
-
-
-    // Member Variables:
-    TextView mCityLabel;
-    ImageView mWeatherImage;
-    TextView mTemperatureLabel;
-
-    // TODO: Declare a LocationManager and a LocationListener here:
-    LocationManager myLocation;
-    LocationListener myListener;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.weather_controller_layout);
-
-        // Linking the elements in the layout to Java code
-        mCityLabel = findViewById(R.id.locationTV);
-        mWeatherImage = findViewById(R.id.weatherSymbolIV);
-        mTemperatureLabel = findViewById(R.id.tempTV);
-        ImageButton changeCityButton = findViewById(R.id.changeCityButton);
-
-
-        // TODO: Add an OnClickListener to the changeCityButton here:
-        changeCityButton.setOnClickListener(view -> {
-            Intent myIntent = new Intent(MainActivity.this, ChangeCityController.class);
-            startActivity(myIntent);
-        });
+        setContentView(R.layout.main_activity_layout);
 
     }
 
-
-    // TODO: Add onResume() here:
-    protected void onResume() {
-        super.onResume();
-        Log.i("onResume", "onResume() fired");
-
-        Intent intent = getIntent();
-        String city= intent.getStringExtra("City");
-
-        if(city != null){
-            getWeatherForNewCity();
-        }else {
-            Log.i("onResume", "onLocationChanged() calling");
-            getWeatherForCurrentLocation();
-        }
-
-    }
-
-
-    // TODO: Add getWeatherForNewCity(String city) here:
-    private void getWeatherForNewCity(){
-        letsDoSomeNetworking();
-    }
-
-
-    // TODO: Add getWeatherForCurrentLocation() here:
-    private void getWeatherForCurrentLocation() {
-        myLocation = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        myListener = new LocationListener() {
-            @Override
-            public void onLocationChanged(Location location) {
-                Log.i("onLocationChanged", "callback received");
-                String latitude = String.valueOf(location.getLatitude());
-                String longitude = String.valueOf(location.getLongitude());
-
-                Log.i("Latitude", latitude);
-                Log.i("Longitude", longitude);
-                //params.put("query",CITY);
-                letsDoSomeNetworking();
-
-            }
-
-            @Override
-            public void onStatusChanged(String s, int i, Bundle bundle) {
-
-            }
-
-            @Override
-            public void onProviderEnabled(String s) {
-
-            }
-
-            @Override
-            public void onProviderDisabled(String s) {
-                Log.i("providerStatus","DISABLED");
-
-            }
-        };
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},REQUEST_CODE);
-            return;
-        }
-        myLocation.requestLocationUpdates(LOCATION_PROVIDER, MIN_TIME, MIN_DISTANCE, myListener);
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if(requestCode == REQUEST_CODE){
-            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                Log.i("requestStatus", "GRANTED");
-            }else{
-                Log.e("requestStatus","DENIED");
-            }
-        }
-    }
-
-    public void letsDoSomeNetworking(){
-    }
-
-
-
-    // TODO: Add updateUI() here:
-    private void updateUI(WeatherDataModel weather){
-        mCityLabel.setText(weather.getmCity());
-        mTemperatureLabel.setText(weather.getmTemp());
-        int resourceID = getResources().getIdentifier(weather.getmIconName(), "drawable", getPackageName());
-        mWeatherImage.setImageResource(resourceID);
-    }
-
-
-
-    // TODO: Add onPause() here:
-
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        if(myLocation != null){
-            myLocation.removeUpdates(myListener);
-        }
-    }
 }
