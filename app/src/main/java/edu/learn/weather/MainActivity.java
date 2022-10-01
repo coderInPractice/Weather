4addc91439775fe51cb0445221b2e1e63331c561
Manish Verma <manish.verma604@gmail.com>
2022-10-01 13:59:12 +0530

    Main screen UI completed.

--- app/src/main/java/edu/learn/weather/MainActivity.java
+++ app/src/main/java/edu/learn/weather/MainActivity.java
@@ -6,15 +6,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Constants:
-    final String WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather";
-    // App ID to use OpenWeather data
-    final String APP_ID = "e72ca729af228beabd5d20e3b7749713";
-    // Time between location updates (5000 milliseconds or 5 seconds)
-    final long MIN_TIME = 5000;
-    // Distance between location updates (1000m or 1km)
-    final float MIN_DISTANCE = 1000;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
